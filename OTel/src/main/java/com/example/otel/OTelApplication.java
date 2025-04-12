e65feafa42d3b494d80297e27b60c6481d7e6f5c
HanJH <71498489+letsgojh0810@users.noreply.github.com>
2025-04-12 16:10:58 +0900

    feat : Add Spring Project

---
+++ OTel/src/main/java/com/example/otel/OTelApplication.java
@@ -0,0 +1,13 @@
+package com.example.otel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OTelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OTelApplication.class, args);
+    }
+
+}
