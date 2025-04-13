d02b8ee01c272c2e67d798fac18514f1feeb52d4
HanJH <71498489+letsgojh0810@users.noreply.github.com>
2025-04-13 22:02:56 +0900

    feat : refact to MSA

---
+++ payment-service/src/main/java/com/example/otel/OTelApplication.java
@@ -0,0 +1,14 @@
+package com.example.otel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OTelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OTelApplication.class, args);
+    }
+
+}
+ 
---
+++ payment-service/src/test/java/com/example/otel/OTelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OTelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
